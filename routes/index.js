3c367c9caaf2356df9e91cf6df2cbb2dd786d0de
Criotech <opesiyanbola8991@gmail.com>
2018-10-22 16:28:43 +1400

    changes mage on the mock up design

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+let studentController = require('../controllers/studentController.js');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', studentController.home);
+
+router.get('/login', studentController.login);
+
+router.get('/profile', studentController.profile);
+
+router.get('/students', studentController.students);
+
+router.get('/main', studentController.main);
+
+router.get('/bursarylogin', studentController.bursarylogin);
+
+router.get('/bursary', studentController.bursary);
+
+
+
+
 
 module.exports = router;
